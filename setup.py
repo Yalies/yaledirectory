9fb4f000f6be2739cf422cc9dd46b2d877d982df
ErikBoesen <me@erikboesen.com>
2019-06-22 23:03:30 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Upload package to PyPi.
+
+from setuptools import setup
+
+setup(name='yaledirectory',
+      version='0.1.0',
+      description='Library for fetching data from the Yale Directory API.',
+      url='https://github.com/ErikBoesen/yaledirectory',
+      author='Erik Boesen',
+      author_email='me@erikboesen.com',
+      license='GPL',
+      packages=['yaledirectory'],
+      install_requires=['requests'])
