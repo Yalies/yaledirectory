18b9e3d25a55fcd48fef0d87420149675e98fea3
ErikBoesen <me@erikboesen.com>
2019-06-24 19:10:31 -0400

    Get netid and password in example from env vars

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 import yaledirectory
+import os
 
 # "api" name can be whatever is most convenient for your program
-api = yaledirectory.YaleDirectory()
+api = yaledirectory.YaleDirectory(os.environ['NETID'], os.environ['YALE_PASSWORD'])
 
 from pprint import pprint
-results = api.search('Erik Boesen')
+results = api.search('George Castillo')
 for person in results:
     print(f'{person.display_name} {person.residential_college_name}')
+print(api.request('Erik Boesen'))
