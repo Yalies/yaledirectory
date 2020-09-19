228c63cef6cf6df2231b0621cbcc6256af5f1271
ErikBoesen <me@erikboesen.com>
2020-09-19 10:18:47 -0600

    Bump to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='yaledirectory',
-      version='1.0.0',
+      version='1.0.1',
       description='Library for fetching data from the Yale Directory API.',
       url='https://github.com/ErikBoesen/yaledirectory',
       author='Erik Boesen',
