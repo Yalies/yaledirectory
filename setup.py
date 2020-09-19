4da573156a4698227a410bb2a3d4d733d66119a8
ErikBoesen <me@erikboesen.com>
2020-09-19 10:55:17 -0600

    Bump to 1.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='yaledirectory',
-      version='1.1.0',
+      version='1.1.1',
       description='Library for fetching data from the Yale Directory API.',
       url='https://github.com/ErikBoesen/yaledirectory',
       author='Erik Boesen',
